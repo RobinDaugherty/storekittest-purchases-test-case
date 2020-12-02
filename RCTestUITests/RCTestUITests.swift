1f404f718065f6203d4ada88b55f326e76435c5d
Robin Daugherty <robin@robindaugherty.net>
2020-12-02 16:36:47 -0500

    UI tests

--- RCTestUITests/RCTestUITests.swift
+++ RCTestUITests/RCTestUITests.swift
@@ -6,37 +6,28 @@
 //
 
 import XCTest
+import StoreKitTest
 
 class RCTestUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testRevenueCat() throws {
         let app = XCUIApplication()
         app.launch()
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let storeKitSession = try! SKTestSession(configurationFileNamed: "Configuration")
+
+        app.buttons["Load with RevenueCat"].tap()
+        XCTAssert(app.staticTexts["$12.99"].waitForExistence(timeout: 5.0))
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    func testNative() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        let storeKitSession = try! SKTestSession(configurationFileNamed: "Configuration")
+
+        app.buttons["Load natively"].tap()
+        XCTAssert(app.staticTexts["$12.99"].waitForExistence(timeout: 5.0))
     }
+
 }
